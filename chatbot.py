10ca4f5e75cdfd275fa313e3b8174bf65e22d254
manasa0000 <manasavabilisetti66@gmail.com>
2025-02-28 10:08:57 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -7,7 +7,7 @@ if "chat" not in st.session_state:
   st.session_state.chat = model.start_chat(history = [])
 st.title("Chatbot-Your AI Assistant")
 st.write("Welcome to the my Chatbot! how can i help you")
-if "messages" not in st.session_messages=[]
+if "messages" not in st.session_state:
 for message in st.session_state.messages:
   with st.chat_message(message["role"])
     st.markdown(message["content"])
